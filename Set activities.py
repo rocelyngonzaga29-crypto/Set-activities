f9d6dc1ad760a887d48ff9caf14447097f54bd99
rocelyngonzaga29-crypto <rocelyngonzaga29@gmail.com>
2026-04-22 08:50:21 +0800

    Add files via upload

---
+++ Set activities.py
@@ -0,0 +1,26 @@
+# Activity 1: Remove Duplicates
+numbers = [1, 2, 2, 3, 4, 4, 5]
+print("Unique numbers:", set(numbers))
+
+
+# Activity 2: Add and Remove
+fruits = {"apple", "banana"}
+fruits.add("orange")
+fruits.remove("banana")
+print("Fruits:", fruits)
+
+
+# Activity 3: Set Operations
+A = {1, 2, 3}
+B = {3, 4, 5}
+
+print("Union:", A | B)
+print("Intersection:", A & B)
+
+
+# Activity 4: Common Students
+classA = {"Rocelyn", "Riana", "Jaharra"}
+classB = {"Jaharra", "Mark", "Cedrick"}
+
+print("Both classes:", classA & classB)
+print("All students:", classA | classB)
